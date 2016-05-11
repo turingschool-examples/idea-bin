e6b28e4ec000dd8797ee1c438b17be9c68cf0173
rrgayhart <rrgayhart@gmail.com>
2016-05-11 05:47:00 -0600

    Set Up Ideas Index

---
+++ app/assets/javascripts/ideas.js
@@ -0,0 +1,20 @@
+function renderIdeasIndex() {
+  var target = $('.ideas');
+  fetchIdeasAndAddThemToThePage(target);
+}
+
+function renderIdea(idea) {
+  return $('<div><h2>' + idea.title + '</h2><p>' + idea.body +'</p></div>').addClass('idea');
+}
+
+function addIdeasToPage(ideas, target) {
+  var renderedIdeas = ideas.map(renderIdea);
+  $(target).append(renderedIdeas);
+}
+
+function fetchIdeasAndAddThemToThePage(target, callback) {
+  return $.getJSON('/ideas').then(function (ideas) {
+    addIdeasToPage(ideas.idea, target);
+    if (typeof callback === 'function') { callback(); }
+  });
+}
